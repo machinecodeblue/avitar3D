a61fdd2a374248f931c1d83d8bbadf20ecf509d6
machinecodeblue <david@machinecodeblue.com>
2016-06-11 11:39:29 -0400

    initial commit

---
+++ world.js
@@ -0,0 +1,26 @@
+var Colors = {
+	red:0xf25346,
+	white:0xd8d0d1,
+	brown:0x59332e,
+	pink:0xF5986E,
+	brownDark:0x23190f,
+	blue:0x68c3c0,
+};
+window.addEventListener('load', init, false);
+
+function init() {
+	// set up the scene, the camera and the renderer
+	createScene();
+
+	// add the lights
+	createLights();
+
+	// add the objects
+	createPlane();
+	createSea();
+	createSky();
+
+	// start a loop that will update the objects' positions 
+	// and render the scene on each frame
+	loop();
+}
